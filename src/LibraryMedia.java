3345b6bf9228c48b6d96cb62269cfafe3dfaa1dc
S33455 <s33455@pjwstk.edu.pl>
2025-04-11 17:20:22 +0200

    Finished on lesson

---
+++ src/LibraryMedia.java
@@ -0,0 +1,15 @@
+public class LibraryMedia {
+    String title;
+    int publicationYear;
+    boolean available;
+
+    public LibraryMedia(String title, int publicationYear, boolean available) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+        this.available = available;
+    }
+
+    public void borrow;
+
+
+}
